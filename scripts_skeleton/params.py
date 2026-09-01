0991aca4b01c94b07699c85a871bf82f8ec534cb
laurabpaulsen <202005791@post.au.dk>
2026-09-01 13:30:59 +0200

    use FileNames from Van Vliet (2020) instead to define paths

--- scripts_skeleton/params.py
+++ scripts_skeleton/params.py
@@ -1,10 +1,44 @@
 from pathlib import Path
+from utils.fnames import FileNames
 
+
+# ------------------------------------------------------------------
+# PREPROCESSING PARAMETERS
+# ------------------------------------------------------------------
+
+## list of bad channels for each subject
+BAD_CHANNELS = {
+    "0164": [],
+}
+
+DATES = {
+    "0164": "20251003_000000",
+}
+
+## Filtering
+L_FREQ = 0.1
+H_FREQ = 40
+ICA_L_FREQ = 1
+FILT_METHOD = "fir"
+
+## Epoching
+EPOCHS_TMIN = -0.2
+EPOCHS_TMAX = 0.7
+BASELINE = (None, 0) # from tmin to time of stimulation
+DOWNSAMPLE_RATE = 250
+
+
+# ------------------------------------------------------------------
+# SOURCE MODELLING
+# ------------------------------------------------------------------
+
+SRC_SPACING = "oct6"
+VOL_SPACING = 10
+
+
+# ------------------------------------------------------------------
 # PATHS
-ROOT = Path("/work")
-RAW_PATH = ROOT / "raw"
-SUBJECTS_DIR = ROOT / "freesurfer"
-
+# ------------------------------------------------------------------
 # determining the user to find a nice place to save derivatives 
 p = Path(__file__)
 
@@ -14,34 +48,33 @@ uCloud_user = next(
 )
 
 
-DERIVATIVES_PATH = ROOT / uCloud_user / "derivatives"
-MEG_PATH = DERIVATIVES_PATH / "MEG"
-
-if not MEG_PATH.exists():
-    MEG_PATH.mkdir(parents=True)
-    print(f"Could not find derivatives path. It has been created at {MEG_PATH}")
+fnames = FileNames()
+fnames.add('root', '/work')
+fnames.add('raw', '{root}/_raw')
 
 
-
-# PREPROCESSING PARAMETERS
-## list of bad channels for each subject
-BAD_CHANNELS = {
-    "sub-001": [],
-    "sub-002": [],
-    #....
-    "sub-nnn":[]
-}
-
-## Filtering
+fnames.add('subjects_dir', '{root}/_freesurfer')
+fnames.add('ucloud_memberfiles', '{root}'+f'/{uCloud_user}')
+fnames.add('derivatives', '{ucloud_memberfiles}/derivatives')
+fnames.add('meg_derivatives', '{derivatives}/MEG')
 
 
-## Epoching
-EPOCHS_TMIN = -0.2
-EPOCHS_TMAX = 0.7
-BASELINE = (None, 0) # from tmin to time of stimulation
-DOWNSAMPLE_RATE = 250
+fnames.add('sub_meg_derivatives', '{meg_derivatives}/{subject}')
+fnames.add('sub_raw', '{raw}/{subject}/{date}/workshop_2025_raw.fif')
 
+# subject level derivatives
+fnames.add('sub_filtered', '{sub_meg_derivatives}/{subject}_filtered_{l_freq}-{h_freq}_{method}_meg.fif')
 
-# SOURCE MODELLING
-SRC_SPACING = "oct6"
-VOL_SPACING = 10
+# reports
+fnames.add('reports', '{derivatives}/reports')
+fnames.add('anatomy_report', '{reports}/{subject}/{subject}_anatomy_report.h5')
+fnames.add('preproc_report', '{reports}/{subject}/{subject}_preproc_report.h5')
+
+# bem
+fnames.add('bem_model', '{sub_meg_derivatives}/{subject}_bem.fif')
+fnames.add('bem_sol', '{sub_meg_derivatives}/{subject}_bem_solution.fif')
+
+# source spaces
+fnames.add('src_surface', '{sub_meg_derivatives}/{subject}_surface-spacing-{surf_spacing}-src.fif')
+fnames.add('src_volume', '{sub_meg_derivatives}/{subject}_volume-spacing-{vol_spacing}mm-src.fif')
+fnames.add('src_combined', '{sub_meg_derivatives}/{subject}_combined-surface-spacing-{surf_spacing}-volume-spacing-{vol_spacing}mm-src.fif')
